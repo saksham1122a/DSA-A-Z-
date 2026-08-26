27c7e38017e39f0f0a5688205c3379f0801e891b
saksham1122a <sakshamnnda01@gmail.com>
2026-08-26 23:20:55 +0530

    Star row column pattern

--- Patterns/Pattern-1.cpp
+++ Patterns/Pattern-1.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include<iostream>
 using namespace std;
 void printPattern(int n){
@@ -13,5 +15,31 @@ int main(){
     cout<<"Enter the number of rows: ";
     cin>>n;
     printPattern(n);
+    return 0;
+}
+
+*/
+
+// if in a question they asked for both rows and columns to be taken from user then we can use below code
+
+#include<iostream>
+using namespace std;
+
+void printPattern(int rows, int cols){
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=cols;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int rows, cols;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+    cout<<"Enter the number of columns: ";
+    cin>>cols;
+    printPattern(rows, cols);
     return 0;
 }
